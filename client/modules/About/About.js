76c93f074dc7ef80db5ccd1deb42f5c411865b29
Filip Stangricki <stangricki@gmail.com>
2018-02-27 16:24:51 +0100

    Initial commit

---
+++ client/modules/About/About.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+const About = () => {
+    return (
+      <div>
+        <h2>Zadanie Kodilli</h2>
+        <a href="/">Back to main page</a>
+      </div>
+    )
+  }
+
+
+export default About;
